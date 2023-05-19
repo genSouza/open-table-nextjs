eeea6a4b40237fa9d471cdceb862f7b76078020d
Genilton Souza <genilton.souza26@gmail.com>
2023-05-19 16:47:20 -0400

    feat(pages): add hardcoded pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'resizer.otstatic.com',
+            port: '',
+            pathname: '/v2/photos/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'images.otstatic.com',
+            port: '',
+            pathname: '/prod1/**',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    fontSize: {
+      "2xs": ".625rem",
+      xsm: ".75rem",
+      sm: ".813rem",
+      reg: ".938rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.375rem",
+      "3xl": "1.563rem",
+      "4xl": "2rem",
+      "5xl": "2.5rem",
+      "6xl": "3.125rem",
+      "7xl": "4.375rem",
     },
   },
   plugins: [],
-}
+};
