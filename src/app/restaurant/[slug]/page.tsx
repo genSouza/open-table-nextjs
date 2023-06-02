3ee1fd2d8f1259ed81da06c24a809a9059ee3ce0
Genilton Souza <genilton.souza26@gmail.com>
2023-06-02 18:03:22 -0400

    feat(restaurant page): add fetch data function

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -5,8 +5,34 @@ import { Description } from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import { PrismaClient } from "@prisma/client";
 
-const RestaurantDetails = () => {
+interface Restaurant {
+  id: string;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const prisma = new PrismaClient();
+const fetchRestaurantBySlug = async (slug: string): Restaurant => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+    // include: {
+    //   cuisine: true,
+    //   location: true,
+    // },
+  });
+  if (!restaurant) {
+    throw new Error("Restaurant not found");
+  }
+
+  return restaurant;
+};
+const RestaurantDetails = async ({ params }: { params: { slug: string } }) => {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  console.log(restaurant);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
